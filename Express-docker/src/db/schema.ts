c717218e57180157cb606b4b0c48a93ac571c272
Rashedin-063 <rashedinislam.06@gmail.com>
2025-10-29 13:13:11 +0600

    express-docker folder deleted

--- Express-docker/src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-import { client } from './../../../dist/index';
-
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name'),
-});
--- Express-docker/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-import config from './app/config';
-
-app.listen(config.port, () => {
-  console.log(`Server listening on port http://localhost:${config.port}`);
-});
